f2b850b77e28d15a6b13475ed7812877dce31700
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2025-04-19 19:47:34 +0000

    bit more the boilerplate

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Add your JavaScript code here
+console.log("Page currently under construction");
+
+// API connection to rightmove (AI generated)
+const API_URL = "https://api.rightmove.co.uk/api/_search";
+
+// API connection to rightmove (from website: https://api-docs.rightmove.co.uk/docs/property-feed-api-product/1/routes/v2/property/commercial/%7Breference%7D/get)
+const API_URL2 = `https://api-services.adftest.rightmove.com/v2/property/commercial/${reference}`;
